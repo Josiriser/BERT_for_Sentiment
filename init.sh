0bba0f4939623140265a0dc3c1716bc402e9f732
josiriser <josiriser@gmail.com>
2020-08-19 11:20:52 +0800

    modified init

--- init.sh
+++ init.sh
@@ -4,3 +4,5 @@ pip install -Ur requirements.txt
 gdown -O trained_model.zip https://drive.google.com/uc?id=1Tu6x9y26GsnQMwWT-6FzVEHEv37wWQ6n
 # unzip
 unzip trained_model.zip
+# delete zip
+rm trained_model.zip
